a82f9014e832cd6d86ad87d1d43fbbaeaa69ffb4
ivo692009 <ivo692009@gmail.com>
2023-04-30 19:21:45 -0300

    mejora con custom commands

--- cypress/e2e/demoblaze-segundo.cy.js
+++ cypress/e2e/demoblaze-segundo.cy.js
@@ -11,81 +11,42 @@
 
 //Esta una referencia para las ayudas para los comandos con cypress
 /// <reference types="cypress" />
+
 //Agregamos esta referencia para utilizar el Xpath como funcion
 require('cypress-xpath');
 
 describe('Demoblaze en partes', () => {
-  it('Alta de usuario + Login + Logout del usuario dado de alta', () => {
+
+
+    beforeEach(() => {
     //Visitamos la pagina
     cy.visit('https://www.demoblaze.com/index.html')
     //Validamos que estemos en la pagina deseada
     cy.title().should("eq", "STORE")
-    //Creamos una variable que vamos a utilizar como usuario y contraseña
-    //en caso de que ya exista solo debemos modificar esta variable para el ejercicio
-
-    let uyc = "pedroperez117"
-
-    //seleccionamos para crear una cuenta
-    cy.get('#signin2').should("be.visible").click()
-    cy.wait(1000)
-    //ponemos el usuario y contraseña y luegos apretamos el boton de crear cuenta
-    cy.get('#sign-username').should("be.visible").type(uyc)
-    //Creamos una espera de un segundo para la carga correta de la informacion
-    cy.wait(1000)
-    cy.get('#sign-password').should("be.visible").type(uyc)
-    cy.wait(1000)
-    cy.get('#signInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').should("be.visible").click()
-    //esperamos un segundo
-    cy.wait(1000)
-    cy.on('window:alert', (str) => {
-      expect(str).to.equal(`This user already exist.`)
-      //Y si el usuario ya existe, lanzamos un mensaje de error forzado
-      throw new Error("El usuario ya existe")
-      cy.visit('https://www.demoblaze.com/index.html')
-      cy.wait(1000)
-
     })
-    //Entramos en la parte de logear para entrar
-    cy.get('#login2').should("be.visible").click()
-    cy.wait(1000)
-    //colocamos el usuario y contraseña que elegimos
-    cy.get('#loginusername').should("be.visible").type(uyc)
-    cy.wait(1000)
-    cy.get('#loginpassword').should("be.visible").type(uyc)
-    cy.wait(1000)
-    cy.get('#logInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').should("be.visible").click()
-    //Validamos que exitosamente el usuario se logea
-    cy.get('#nameofuser').should("be.visible").contains(uyc)
-
-    //Realizamos el logout del usuario
-    cy.get('#logout2').should("be.visible").click()
-    cy.wait(1000)
-    // //validamos que aparesca el boton de "sing in" una ves deslogeado el usuario
-    cy.get('#login2').should("be.visible").contains("Log in")
-
-    //En este punto ya estamos deslogeados y realizado el ejercicio
+  
+  it.only('Alta de usuario + Login + Logout del usuario dado de alta', () => {
+    
+    //Creamos una variable que vamos a utilizar como usuario y contraseña
+    let usuario="JosueJosue2"
+    let contra="PedroPascal2"
+    //Sing Up pasando por parametros usuario y contraseña
+    cy.SingUp(usuario,contra)
+    //Log In pasando por parametros usuario y contrañse
+    cy.LogInU(usuario,contra)
+    //Deslogeamos
+    cy.LogOutU();
+   
   })
 
   it('Alta de usuario + Login + Compra de laptop + Logout del usuario dado de alta', () => {
-    //Visitamos la pagina
-    cy.visit('https://www.demoblaze.com/index.html')
-    //Validamos que estemos en la pagina deseada
-    cy.title().should("eq", "STORE")
-    //En este caso vamos a utilizar un usuario y contraseña que ya conocemos, podemos utilizar el del spect anterior
+    
 
-    let uyc = "pedroperez117"
+    //Creamos una variable que vamos a utilizar como usuario y contraseña
+    let usuario="JosueJosue2"
+    let contra="PedroPascal2"
 
-    //Entramos en la parte de logear para entrar
-    cy.get('#login2').should("be.visible").click()
-    cy.wait(1000)
-    //colocamos el usuario y contraseña que elegimos
-    cy.get('#loginusername').should("be.visible").type(uyc)
-    cy.wait(1000)
-    cy.get('#loginpassword').should("be.visible").type(uyc)
-    cy.wait(1000)
-    cy.get('#logInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').should("be.visible").click()
-    //Validamos que exitosamente el usuario se logea
-    cy.get('#nameofuser').should("be.visible").contains(uyc)
+    cy.LogInU(usuario,contra)
 
     //Seleccionamos el apartado de Laptops en la barra lateral 
     cy.xpath("/html/body/div[5]/div/div[1]/div/a[3]").should("be.visible").click()
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,54 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('SingUp', (username, password) => { 
+
+    cy.get('#signin2').should("be.visible").click()
+    cy.wait(1000)
+    //ponemos el usuario y contraseña y luegos apretamos el boton de crear cuenta
+    cy.get('#sign-username').should("be.visible").type(username)
+    //Creamos una espera de un segundo para la carga correta de la informacion
+    cy.wait(1000)
+    cy.get('#sign-password').should("be.visible").type(password)
+    cy.wait(1000)
+    cy.get('#signInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').should("be.visible").click()
+    //esperamos un segundo
+    cy.wait(1000)
+    cy.on('window:alert', (str) => {
+
+        if(str == 'This user already exist.'){
+            //Y si el usuario ya existe, lanzamos un mensaje de error forzado
+            throw new Error("El usuario ya existe")
+            cy.visit('https://www.demoblaze.com/index.html')
+            cy.wait(1000)
+        }
+        //expect(str).to.equal(`This user already exist.`)
+        
+        })
+    })
+
+Cypress.Commands.add('LogInU', (username, password) => { 
+    //Entramos en la parte de logear para entrar
+    cy.get('#login2').should("be.visible").click()
+    cy.wait(1000)
+    //colocamos el usuario y contraseña que elegimos
+    cy.get('#loginusername').should("be.visible").type(username)
+    cy.wait(1000)
+    cy.get('#loginpassword').should("be.visible").type(password)
+    cy.wait(1000)
+    cy.get('#logInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').should("be.visible").click()
+    //Validamos que exitosamente el usuario se logea
+    cy.get('#nameofuser').should("be.visible").contains(username)
+
+})
+
+Cypress.Commands.add('LogOutU',() => { 
+
+    cy.get('#logout2').should("be.visible").click()
+    cy.wait(1000)
+    // //validamos que aparesca el boton de "sing in" una ves deslogeado el usuario
+    cy.get('#login2').should("be.visible").contains("Log in")
+
+    //En este punto ya estamos deslogeados y realizado el ejercicio
+  })
